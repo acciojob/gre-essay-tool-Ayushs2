70f5bb9e2086b72ee8024ff8a3825634ff907ae1
Ayushs2 <singh.ayush0105@gmail.com>
2022-12-27 14:07:22 +0530

    updated

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-//your code here
+let etext=document.getElementById("evaluatedText");
+let countw=document.getElementById("wordCount");
+
+var c=0;
+
+etext.addEventListener("keydown",(e)=>{
+    if(e.key!='Backspace' && c>=0){
+    c++;
+    countw.innerHTML=c;
+    }
+    else{
+        if(c)
+        c--;
+        countw.innerHTML=c;
+        
+    }
+
+})
